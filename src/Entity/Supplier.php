52a8343a1e6dedcb99fed7a1b0abcb52f33b8d57
Life1223pl <daw_pl@interia.pl>
2022-07-03 23:18:08 +0200

    Added new supplier resource and CRUD for it

---
+++ src/Entity/Supplier.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="app_supplier")
+ */
+class Supplier implements SupplierInterface
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(type="string")
+     */
+    private $email;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string|null $name
+     */
+    public function setName(string|null $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string|null $email
+     */
+    public function setEmail(string|null $email): void
+    {
+        $this->email = $email;
+    }
+
+}
---
+++ src/Entity/SupplierInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Entity;
+
+
+use Sylius\Component\Resource\Model\ResourceInterface;
+
+interface SupplierInterface extends ResourceInterface
+{
+
+    /**
+     * @return ?string
+     */
+    public function getName(): ?string;
+
+    /**
+     * @param ?string $name
+     */
+    public function setName(?string $name): void;
+
+    /**
+     * @return ?string
+     */
+    public function getEmail(): ?string;
+
+    /**
+     * @param ?string $email
+     */
+    public function setEmail(?string $email): void;
+}
