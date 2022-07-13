bfd812f387301ffb0e186be16de3511662270c1a
Life1223pl <daw_pl@interia.pl>
2022-07-13 11:06:53 +0200

    ReviewCreationListener, Trusted Users Group For Adding Reviews without checking

---
+++ src/Listener/ReviewCreationListener.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Listener;
+
+use App\Checker\TrustedCustomerCheckerInterface;
+use SM\Factory\FactoryInterface;
+use SM\SMException;
+use Sylius\Component\Core\Model\CustomerInterface;
+use Sylius\Component\Core\ProductReviewTransitions;
+use Sylius\Component\Review\Model\ReviewInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+class ReviewCreationListener
+{
+    /** @var FactoryInterface */
+    private $stateMachineFactory;
+
+    /** @var TrustedCustomerCheckerInterface */
+    private $trustedCustomerChecker;
+
+    /**
+     * @param FactoryInterface $stateMachineFactory
+     * @param TrustedCustomerCheckerInterface $trustedCustomerChecker
+     */
+    public function __construct(FactoryInterface $stateMachineFactory, TrustedCustomerCheckerInterface $trustedCustomerChecker)
+    {
+        $this->stateMachineFactory = $stateMachineFactory;
+        $this->trustedCustomerChecker = $trustedCustomerChecker;
+    }
+
+
+    /**
+     * @throws SMException
+     */
+    public function acceptForTrustedAuthor(GenericEvent $event): void
+    {
+      /** @var ReviewInterface $review */
+      $review = $event->getSubject();
+
+      /** @var CustomerInterface $author */
+      $author = $review->getAuthor();
+
+      if(!$this->trustedCustomerChecker->check($author)){
+          return;
+      }
+
+      $stateMachine = $this->stateMachineFactory->get($review, ProductReviewTransitions::GRAPH);
+      $stateMachine->apply(ProductReviewTransitions::TRANSITION_ACCEPT);
+
+
+    }
+
+
+}
